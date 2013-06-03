263bdb92c6423ffa136fe619909a4923352ee7d2
t6631696 <t6631696@tlp-t-174.talpiot.huji.ac.il>
2013-06-03 19:52:56 +0300

    d

--- clids/ex4/exceptions/NoReturnStatementException.java
+++ clids/ex4/exceptions/NoReturnStatementException.java
@@ -2,4 +2,9 @@ package clids.ex4.exceptions;
 
 public class NoReturnStatementException extends Exception {
 
+	private static final long serialVersionUID = 1L;
+
+	public NoReturnStatementException(){
+		super("NoReturnStatementException");
+	}
 }
