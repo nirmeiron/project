6ed49b71f7f8be79d602ae5d4d7b3d4d65797048
t6631696 <t6631696@tlp-t-174.talpiot.huji.ac.il>
2013-06-04 01:29:29 +0300

    README

--- clids/ex4/exceptions/TypeAssignmentException.java
+++ clids/ex4/exceptions/TypeAssignmentException.java
@@ -2,9 +2,6 @@ package clids.ex4.exceptions;
 
 public class TypeAssignmentException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	public TypeAssignmentException(){
 		super("TypeAssignmentException");
